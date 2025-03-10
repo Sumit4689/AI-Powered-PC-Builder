a43264171f64227e5afdf951d5d4f856212f000f
Sumit Subhash Patil <sumitgatpatil41@gmail.com>
2025-03-10 22:30:47 +0530

    dataBase Connection and .env setUp

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 2002;
+const cors = require('cors');
+const connectDb = require('./config/dbConnection');
+connectDb();
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 
 
 
-app.listen(3000, () => {    
-    console.log('Server is listening on http://localhost:3000');
+app.listen(port, () => {    
+    console.log(`Server is running on the port: ${port}`);
+    console.log(`http://localhost:${port}`);
 });
